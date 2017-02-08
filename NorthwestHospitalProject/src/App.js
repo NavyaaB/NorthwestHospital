33a2453aba95ced3de1ec2976be4347f7be51966
Navya2510 <navyabachati@gmail.com>
2017-02-08 13:58:25 -0600

    Navbar

--- NorthwestHospitalProject/src/App.js
+++ NorthwestHospitalProject/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import {Navbar,ButtonToolbar,Button,Nav,Image} from 'react-bootstrap'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+
+              <a href="#" id="name"> Northwest Hospital</a>
+
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <ButtonToolbar>
+                <Button>Sign in</Button>
+                <Button bsStyle="info">Sign up</Button>
+                </ButtonToolbar>
+            </Nav>
+
+          </Navbar.Collapse>
+        </Navbar>
       </div>
     );
   }
